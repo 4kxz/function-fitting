f0fcb2aaf6b78a32fcbed9a3a14a0ba90bf8a5d4
Alkxzv <carlos@lkxz.net>
2018-02-24 17:58:37 +0100

    Created state object

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 
 app = Flask(__name__)
 
-COLOR = '#F00', '#B00', '#800'
+COLOR = '#F00', '#B00', '#B00', '#B00', '#B00'
 
 
 @app.route('/')
@@ -16,24 +16,15 @@ def static_files(path):
     return send_from_directory('static', path)
 
 
-def process_request():
-    points = request.json['points']
-    return {
-        'domain': request.json['domain'],
-        'x': [p['x'] for p in points],
-        'y': [p['y'] for p in points],
-    }
-
-
 @app.route('/polynomial/<int:degree>', methods=['POST'])
 def polynomial(degree):
-    data = process_request()
-    z = np.polyfit(data['x'], data['y'], degree)
-    f = np.poly1d(z)
-    x = np.linspace(*data['domain'], 100)
-    y = f(x)
+    data = request.json
+    x = [p['x'] for p in data['points']]
+    y = [p['y'] for p in data['points']]
+    p = np.poly1d(np.polyfit(x, y, degree))
+    x_min, x_max = data['domain']['x']
     return jsonify({
-        'index': degree - 1,
+        'degree': degree,
         'color': COLOR[degree - 1],
-        'path': [{'x': x, 'y': y} for x, y in zip(x, y)],
+        'path': [{'x': x, 'y': p(x)} for x in np.linspace(x_min, x_max, 100)],
     })
--- static/app.js
+++ static/app.js
@@ -1,19 +1,29 @@
 
-var state = {
-    view: {
-        size: 500,
-        pad: 40,
-        fmt: d3.format(".3n"),
-        x: {
-            domain: [0, 10],
-            axis: d3.axisBottom,
-            transform: (size, pad) => `translate(0, ${size - pad})`,
-        },
-        y: {
-            domain: [10, 0],
-            axis: d3.axisLeft,
-            transform: (size, pad) => `translate(${pad}, 0)`,
-        },
+var CONF = {
+    pad: 40,
+    fmt: d3.format(".3n"),
+    size: function() {
+        let width = document.getElementById("view").offsetWidth;
+        let height = document.getElementById("view").offsetHeight;
+        return Math.min(width, height);
+    },
+    range: function() {
+        return [this.pad, this.size() - this.pad];
+    },
+    axes: [{
+        axis: d3.axisBottom,
+        transform: (size, pad) => `translate(0, ${size - pad})`,
+    }, {
+        axis: d3.axisLeft,
+        transform: (size, pad) => `translate(${pad}, 0)`,
+    }],
+
+};
+
+var DATA = {
+    domain: {
+        x: [0, 10],
+        y: [10, 0],
     },
     points: [
         {x: 2, y: 2},
@@ -24,92 +34,90 @@ var state = {
     fits: [],
 };
 
-computeSize(state);
-drawView(state.view);
-drawPoints(state);
 
-function computeSize(state) {
-    let width = document.getElementById("view").offsetWidth;
-    let height = document.getElementById("view").offsetHeight;
-    state.view.size = Math.min(width, height);
-    state.view.range = [state.view.pad, state.view.size - state.view.pad];
-}
+drawView(DATA, CONF);
+drawPoints(DATA, CONF);
+updateFits(DATA, CONF);
 
-function drawView(state) {
+d3.select("svg").on('click', function() {
+    let [x, y] = d3.mouse(this);
+    DATA.points.push({
+        x: CONF.axes[0].scale.invert(x),
+        y: CONF.axes[1].scale.invert(y),
+    });
+    drawPoints(DATA, CONF);
+    updateFits(DATA, CONF);
+});
+
+function updateFits(data, conf) {
+    Promise.all([
+        getFit('/polynomial/1', data),
+        getFit('/polynomial/2', data),
+        getFit('/polynomial/4', data),
+    ]).then(results => {
+        data.fits = results;
+        drawFits(data, conf);
+    });
+};
+
+function getFit(url, data) {
+    var request = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    };
+    return fetch(url, request).then(response => response.json());
+};
+
+function drawView(data, conf) {
     var view = d3.select("#view");
+    var size = conf.size();
     var svg = view.selectAll("svg")
-        .data([state])
+        .data([conf])
         .enter()
         .append("svg")
-        .attr("width", d => d.size)
-        .attr("height", d => d.size)
+        .attr("width", d => size)
+        .attr("height", d => size)
     ;
     var axes = svg.selectAll(".axis")
-        .data([state.x, state.y])
+        .data(conf.axes)
         .enter()
         .append("g")
         .attr("class", "axis")
-        .attr("transform", d => d.transform(state.size, state.pad))
-        .each(function(d) {
-            d.scale = d3.scaleLinear().domain(d.domain).range(state.range);
+        .attr("transform", d => d.transform(size, conf.pad))
+        .each(function(d, i) {
+            let domain = data.domain['xy'[i]];
+            let range = conf.range();
+            d.scale = d3.scaleLinear().domain(domain).range(range);
             d.axis(d.scale)(d3.select(this));
         })
     ;
 };
 
-// var svgPoints = svg.append("g")
-//     .attr("class", "points")
-// ;
-//
-// var svgFits = svg.append("g")
-//     .attr("class", "fits")
-// ;
-//
-// var table = d3.select("body")
-//     .append("div")
-//     .attr("class", "panel")
-//     .append("table")
-//     ;
-//
-// svg.append("g")
-//     .attr("class", "axis")
-//     .attr("transform", `translate(0, ${chart.size - chart.pad})`)
-//     .call(x.axis)
-// ;
-//
-// svg.append("g")
-//     .attr("class", "axis")
-//     .attr("transform", `translate(${chart.pad}, 0)`)
-//     .call(y.axis)
-// ;
-//
-// var fitLine = d3.line()
-//     .x(d => x.scale(d.x))
-//     .y(d => y.scale(d.y))
-// ;
-
-function drawPoints(state) {
+function drawPoints(data, conf) {
     var svg = d3.select("svg");
-    var circles = svg.selectAll("circle").data(state.points);
-    circles.enter().append("circle")
+    var circles = svg.selectAll("circle.point").data(data.points);
+    circles.enter()
+        .append("circle")
         .attr("class", "point")
-        .attr("r", 4)
-        .attr("cx", d => state.view.x.scale(d.x))
-        .attr("cy", d => state.view.y.scale(d.y))
+        .attr("r", 3)
+        .attr("cx", d => conf.axes[0].scale(d.x))
+        .attr("cy", d => conf.axes[1].scale(d.y))
     ;
     circles.exit().remove();
-    // var rows = table.selectAll("tr").data(data).enter()
-    //     .append("tr")
-    // ;
-    // var cells = rows.selectAll("td").data(d => [d.x, d.y]).enter()
-    //     .append("td")
-    //     .text(d => fmt(d))
-    // ;
 };
 
-function drawFits(data) {
-    var paths = svgFits.selectAll("path").data(data);
+function drawFits(data, conf) {
+    var svg = d3.select("svg");
+    var paths = svg.selectAll("path.fit").data(data.fits);
+    var fitLine = d3.line()
+        .x(d => conf.axes[0].scale(d.x))
+        .y(d => conf.axes[1].scale(d.y))
+    ;
     paths.enter().append("path")
+        .attr("class", "fit")
         .attr("stroke", d => d.color)
         .attr("d", d => fitLine(d.path))
     ;
@@ -119,41 +127,3 @@ function drawFits(data) {
     ;
     paths.exit().remove();
 };
-
-function updateFits(error, response) {
-    console.log(response.color);
-    console.log(fits);
-    fits[response.index] = response;
-    drawFits(fits);
-};
-
-function update() {
-    var postData = JSON.stringify({
-        domain: x.domain,
-        points: points,
-    });
-    d3.json('/polynomial/1')
-        .header("Content-Type", "application/json")
-        .post(postData, updateFits)
-    ;
-    d3.json('/polynomial/2')
-        .header("Content-Type", "application/json")
-        .post(postData, updateFits)
-    ;
-    d3.json('/polynomial/3')
-        .header("Content-Type", "application/json")
-        .post(postData, updateFits)
-    ;
-    drawPoints(points);
-}
-
-// update();
-//
-// svg.on('click', function() {
-//     let coord = d3.mouse(this);
-//     points.push({
-//         x: x.scale.invert(coord[0]),
-//         y: y.scale.invert(coord[1]),
-//     });
-//     update();
-// });
