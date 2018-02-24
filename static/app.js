d95254bd5011699bbd6b57a929ee029a6b856a30
Alkxzv <carlos@lkxz.net>
2018-02-24 11:19:37 +0100

    Managing state

--- static/app.js
+++ static/app.js
@@ -1,83 +1,110 @@
-var size = 740;
-var pad = 40;
-var fmt = d3.format(".3n");
-var points = [
-    {x: 2, y: 2},
-    {x: 5, y: 4},
-    {x: 7, y: 5},
-    {x: 8, y: 4},
-];
-var fits = [];
 
-var x = {
-    domain: [0, 10],
-    range: [pad, size - pad],
+var state = {
+    view: {
+        size: 500,
+        pad: 40,
+        fmt: d3.format(".3n"),
+        x: {
+            domain: [0, 10],
+            axis: d3.axisBottom,
+            transform: (size, pad) => `translate(0, ${size - pad})`,
+        },
+        y: {
+            domain: [10, 0],
+            axis: d3.axisLeft,
+            transform: (size, pad) => `translate(${pad}, 0)`,
+        },
+    },
+    points: [
+        {x: 2, y: 2},
+        {x: 5, y: 4},
+        {x: 7, y: 5},
+        {x: 8, y: 4},
+    ],
+    fits: [],
 };
-var y = {
-    domain: [10, 0],
-    range: [pad, size - pad],
-};
-x.scale = d3.scaleLinear().domain(x.domain).range(x.range);
-y.scale = d3.scaleLinear().domain(y.domain).range(y.range);
-x.axis = d3.axisBottom(x.scale);
-y.axis = d3.axisLeft(y.scale);
 
-var svg = d3.select("body")
-    .append("div")
-    .attr("class", "view")
-    .append("svg")
-    .attr("width", size)
-    .attr("height", size)
-;
+computeSize(state);
+drawView(state.view);
+drawPoints(state);
 
-var svgPoints = svg.append("g")
-    .attr("class", "points")
-;
+function computeSize(state) {
+    let width = document.getElementById("view").offsetWidth;
+    let height = document.getElementById("view").offsetHeight;
+    state.view.size = Math.min(width, height);
+    state.view.range = [state.view.pad, state.view.size - state.view.pad];
+}
 
-var svgFits = svg.append("g")
-    .attr("class", "fits")
-;
-
-var table = d3.select("body")
-    .append("div")
-    .attr("class", "panel")
-    .append("table")
+function drawView(state) {
+    var view = d3.select("#view");
+    var svg = view.selectAll("svg")
+        .data([state])
+        .enter()
+        .append("svg")
+        .attr("width", d => d.size)
+        .attr("height", d => d.size)
     ;
+    var axes = svg.selectAll(".axis")
+        .data([state.x, state.y])
+        .enter()
+        .append("g")
+        .attr("class", "axis")
+        .attr("transform", d => d.transform(state.size, state.pad))
+        .each(function(d) {
+            d.scale = d3.scaleLinear().domain(d.domain).range(state.range);
+            d.axis(d.scale)(d3.select(this));
+        })
+    ;
+};
 
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", `translate(0, ${size - pad})`)
-    .call(x.axis)
-;
+// var svgPoints = svg.append("g")
+//     .attr("class", "points")
+// ;
+//
+// var svgFits = svg.append("g")
+//     .attr("class", "fits")
+// ;
+//
+// var table = d3.select("body")
+//     .append("div")
+//     .attr("class", "panel")
+//     .append("table")
+//     ;
+//
+// svg.append("g")
+//     .attr("class", "axis")
+//     .attr("transform", `translate(0, ${chart.size - chart.pad})`)
+//     .call(x.axis)
+// ;
+//
+// svg.append("g")
+//     .attr("class", "axis")
+//     .attr("transform", `translate(${chart.pad}, 0)`)
+//     .call(y.axis)
+// ;
+//
+// var fitLine = d3.line()
+//     .x(d => x.scale(d.x))
+//     .y(d => y.scale(d.y))
+// ;
 
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", `translate(${pad}, 0)`)
-    .call(y.axis)
-;
-
-var fitLine = d3.line()
-    .x(d => x.scale(d.x))
-    .y(d => y.scale(d.y))
-;
-
-function drawPoints(data) {
-    var circles = svgPoints.selectAll("circle").data(data);
+function drawPoints(state) {
+    var svg = d3.select("svg");
+    var circles = svg.selectAll("circle").data(state.points);
     circles.enter().append("circle")
         .attr("class", "point")
         .attr("r", 4)
-        .attr("cx", d => x.scale(d.x))
-        .attr("cy", d => y.scale(d.y))
+        .attr("cx", d => state.view.x.scale(d.x))
+        .attr("cy", d => state.view.y.scale(d.y))
     ;
     circles.exit().remove();
-    var rows = table.selectAll("tr")
-        .data(data)
-        .enter()
-        .append("tr");
-    var cells = rows.selectAll("td")
-        .data(d => [d.x, d.y])
-        .enter().append("td")
-        .text(d => fmt(d));
+    // var rows = table.selectAll("tr").data(data).enter()
+    //     .append("tr")
+    // ;
+    // var cells = rows.selectAll("td").data(d => [d.x, d.y]).enter()
+    //     .append("td")
+    //     .text(d => fmt(d))
+    // ;
 };
 
 function drawFits(data) {
@@ -120,13 +147,13 @@ function update() {
     drawPoints(points);
 }
 
-update();
-
-svg.on('click', function() {
-    let coord = d3.mouse(this);
-    points.push({
-        x: x.scale.invert(coord[0]),
-        y: y.scale.invert(coord[1]),
-    });
-    update();
-});
+// update();
+//
+// svg.on('click', function() {
+//     let coord = d3.mouse(this);
+//     points.push({
+//         x: x.scale.invert(coord[0]),
+//         y: y.scale.invert(coord[1]),
+//     });
+//     update();
+// });
